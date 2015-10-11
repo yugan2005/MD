7eea42abc9e8fd3a11294d47d89b918e61f72345
yugan2005 <yugan2005@gmail.com>
2015-10-11 08:05:40 -0700

    Merge to desktop

---
+++ test/edu/MD/modeling/IInteractorTest.java
@@ -0,0 +1,14 @@
+package edu.MD.modeling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IInteractorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
