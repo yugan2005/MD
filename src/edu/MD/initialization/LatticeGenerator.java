bb01d7c7bc5cbada44fab84773d3bbf8c81d0d23
yugan2005 <yugan2005@gmail.com>
2015-11-11 07:42:08 -0800

    Start working on Initialization module

---
+++ src/edu/MD/initialization/LatticeGenerator.java
@@ -0,0 +1,15 @@
+package edu.MD.initialization;
+
+import edu.MD.number.MDNumber;
+import edu.MD.utilityBD.MDConstants;
+
+public class LatticeGenerator {
+	private MDNumber molarDensity;
+	private MDNumber latticeLength = getLatticeLength();
+	
+	public MDNumber getLatticeLength(){
+		double NA = MDConstants.AVOGADRO;
+		return 
+	}
+
+}
