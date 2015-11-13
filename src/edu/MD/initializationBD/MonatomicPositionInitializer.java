775dbee116b0c44317a1e95bebcff5ab4d1540f8
yugan2005 <yugan2005@gmail.com>
2015-11-12 22:59:28 -0800

    Liquid density is done. Working on vapor matching

--- src/edu/MD/initializationBD/MonatomicPositionInitializer.java
+++ src/edu/MD/initializationBD/MonatomicPositionInitializer.java
@@ -14,40 +14,32 @@ public class MonatomicPositionInitializer {
 
 	public MonatomicPositionInitializer(String name, int filmThickness, int filmSize, int vaporLength,
 			double temperature) {
-		if (filmThickness<1) throw new IllegalArgumentException("Film thickness should be at least 1");
-		
+		if (filmThickness < 1)
+			throw new IllegalArgumentException("Film thickness should be at least 1");
+
 		this.filmThickness = filmThickness;
 		this.filmSize = filmSize;
 		this.vaporLength = vaporLength;
-		
+
 		vaporMolarDensity = MDConstants.getMolarDensity(name, temperature, "vapor");
 		liquidMolarDensity = MDConstants.getMolarDensity(name, temperature, "liquid");
-		
+
 		liquidLatticeLength = calLiquidLatticeLength();
-
-		
+		MDNumber systemSize = liquidLatticeLength.times(filmSize);
 
 	}
-	
-	private MDNumber calLiquidLatticeLength(){
-		MDNumber liquidLatticeLength;
-		switch (filmSize) {
-		case 1:
-			liquidLatticeLength = liquidMolarDensity.times(MDConstants.AVOGADRO).pow(-1).times(6).pow(1.0 / 3.0);
-			break;
-		default:
-			liquidLatticeLength = liquidMolarDensity.times(MDConstants.AVOGADRO).pow(-1)
-					.times((4 * filmThickness + 2) / ((double) filmThickness)).pow(1.0 / 3.0);
-		}
-		return liquidLatticeLength;
+
+	private MDNumber calLiquidLatticeLength() {
+		// Refer to the attached notes
+		return liquidMolarDensity.times(MDConstants.AVOGADRO).pow(-1)
+				.times((4 * filmThickness + 2) / ((double) filmThickness)).pow(1.0 / 3.0);
 	}
-	
-	private MDNumber calVaporLatticeLength(){
+
+	private MDNumber calVaporLatticeLength() {
 		MDNumber liquidLatticeLength;
 		// TODO to here
 		return null;
 
 	}
-
 
 }
