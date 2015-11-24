b56722c1f623662c10aa3599f2793aaca03b802a
yugan2005 <yugan2005@gmail.com>
2015-11-24 10:03:55 -0800

    Working on density profile

---
+++ src/edu/MD/statThermodynamic/MonatomicAxialBasedOnParticleDensityCalculator.java
@@ -0,0 +1,14 @@
+package edu.MD.statThermodynamic;
+
+import edu.MD.number.MDNumber;
+import edu.MD.number.NumberFactory;
+import edu.MD.utility.MDVector;
+
+public class MonatomicAxialBasedOnParticleDensityCalculator {
+	private static final NumberFactory numFactory = NumberFactory.getInstance();
+	
+	public static MDNumber calculate(Iterable<MDVector> positions, MDNumber mass) {
+
+	
+
+}
--- src/edu/MD/statThermodynamic/MonatomicSysTemperatureCalculator.java
+++ src/edu/MD/statThermodynamic/MonatomicSysTemperatureCalculator.java
@@ -1,9 +1,8 @@
 package edu.MD.statThermodynamic;
 
-import java.util.Collection;
-
 import edu.MD.modeling.Particle;
 import edu.MD.number.MDNumber;
+import edu.MD.utility.IterableCounter;
 import edu.MD.utility.MDConstants;
 import edu.MD.utility.MDVector;
 
@@ -15,30 +14,19 @@ public class MonatomicSysTemperatureCalculator {
 		Particle sampleParticle = particles.iterator().next();
 		int DOF = sampleParticle.getDOF();
 
-		int counter = iterableSize(particles);
+		int counter = IterableCounter.count(particles);
 
 		return kinecticE.times(2.0 / (counter * DOF * MDConstants.KB));
 	}
 
 	public static MDNumber calculate(Iterable<MDVector> velocities, String name) {
 		MDNumber kinecticE = MonatomicSysKineticECalculator.calculate(velocities, MDConstants.getMass(name));
-		int counter = iterableSize(velocities);
+		int counter = IterableCounter.count(velocities);
 		int DOF = MDConstants.getDOF(name);
 
 		return kinecticE.times(2.0 / (counter * DOF * MDConstants.KB));
 	}
 
-	private static int iterableSize(Iterable<?> iterable) {
-		// This is the trick used by Iterables.size(Iterable) of Guava
-		int counter = 0;
-		if (iterable instanceof Collection<?>) {
-			counter = ((Collection<?>) iterable).size();
-		} else {
-			for (@SuppressWarnings("unused")
-			Object object : iterable)
-				counter++;
-		}
-		return counter;
-	}
+
 
 }
