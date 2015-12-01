8a733810cdc298cad8fe3c388bc94ee7f5dbb9f2
yugan2005 <yugan2005@gmail.com>
2015-12-01 15:42:13 -0800

    Add totalMass verification test case and passed

--- test/edu/MD/statThermodynamic/MonatomicAxialBasedOnParticleDensityCalculatorTest.java
+++ test/edu/MD/statThermodynamic/MonatomicAxialBasedOnParticleDensityCalculatorTest.java
@@ -3,6 +3,7 @@ package edu.MD.statThermodynamic;
 import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -16,15 +17,18 @@ import edu.MD.globalSetting.PBCBoundarySetting;
 import edu.MD.initialization.MonatomicPositionInitializer;
 import edu.MD.number.MDNumber;
 import edu.MD.number.MDVector;
+import edu.MD.number.NumberFactory;
 import edu.MD.utility.MDConstants;
 
 public class MonatomicAxialBasedOnParticleDensityCalculatorTest {
 	private Iterable<MDVector> positions;
+	private int totalNumParticles;
 	private MDVector systemBoundary;
 	private static String name;
 	private double temperature;
 	private int filmSize, filmThickness, vaporOneSideThickness;
 	private List<List<MDNumber>> densityProfile;
+	
 
 	@BeforeClass
 	public static void globalInit() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
@@ -43,6 +47,7 @@ public class MonatomicAxialBasedOnParticleDensityCalculatorTest {
 				filmSize, vaporOneSideThickness, temperature);
 		systemBoundary = positionInitializer.getSystemBoundary();
 		positions = positionInitializer.getPositions();
+		totalNumParticles = positionInitializer.getTotalNumberOfParticles();
 
 		PBCBoundarySetting.set(systemBoundary);
 
@@ -72,6 +77,26 @@ public class MonatomicAxialBasedOnParticleDensityCalculatorTest {
 		System.out.println(String.format("TotalY: %.3e", systemBoundary.getCartesianComponent()[1].toDouble()));
 		System.out.println(String.format("vapor density: %.3e", MDConstants.getMolarDensity(name, temperature, "vapor")));
 		System.out.println(String.format("liquid density: %.3e", MDConstants.getMolarDensity(name, temperature, "liquid")));
+	}
+	
+	@Test
+	public void densityProfilePerserveTotalMass() {
+		MDNumber crossSection = systemBoundary.getCartesianComponent()[0].times(systemBoundary.getCartesianComponent()[2]);
+		List<MDNumber> yPositions = densityProfile.get(0);
+		List<MDNumber> densityValues = densityProfile.get(1);
+		MDNumber numParticles = NumberFactory.getInstance().valueOf(0);
+
+		for (int i = 0; i < yPositions.size(); i++) {
+			MDNumber yStart = yPositions.get((i+yPositions.size()-1)%yPositions.size());
+			MDNumber yEnd = yPositions.get((i+yPositions.size()+1)%yPositions.size());
+			if (yEnd.compareTo(yStart)<0){
+				yEnd = yEnd.plus(systemBoundary.getCartesianComponent()[1]);
+			}
+			MDNumber yLength = yEnd.minus(yStart);
+			numParticles = numParticles.plus(yLength.times(crossSection).times(densityValues.get(i).times(MDConstants.AVOGADRO)));
+		}
+		
+		assertThat(totalNumParticles, equalTo(numParticles.round()));
 	}
 
 }
