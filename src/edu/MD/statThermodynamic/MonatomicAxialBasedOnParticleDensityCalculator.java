2a246ed9dc93f8830c7d7851d4a320b557e6f22e
yugan2005 <yugan2005@gmail.com>
2015-12-01 15:50:44 -0800

    Merged the new density profile calculator

--- src/edu/MD/statThermodynamic/MonatomicAxialBasedOnParticleDensityCalculator.java
+++ src/edu/MD/statThermodynamic/MonatomicAxialBasedOnParticleDensityCalculator.java
@@ -48,56 +48,6 @@ public class MonatomicAxialBasedOnParticleDensityCalculator {
 
 		listOfPositions.sort(new YComparator());
 
-package edu.MD.statThermodynamic;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import edu.MD.number.MDNumber;
-import edu.MD.number.MDVector;
-import edu.MD.utility.IterableCounter;
-import edu.MD.utility.MDConstants;
-
-public class MonatomicAxialBasedOnParticleDensityCalculator {
-
-	private static class YComparator implements Comparator<MDVector> {
-
-		@Override
-		public int compare(MDVector vector1, MDVector vector2) {
-			MDNumber y1 = vector1.getCartesianComponent()[1];
-			MDNumber y2 = vector2.getCartesianComponent()[1];
-
-			return y1.compareTo(y2);
-		}
-
-	}
-
-	/**
-	 * @param positions
-	 *            Iterable<MDVector>
-	 * @param systemBoundary
-	 * @return DensityProfiles in List<List<MDNumber>> format. Two lists, the
-	 *         1st list is the y location, the 2nd list is the corresponding
-	 *         local density value.
-	 */
-	public static List<List<MDNumber>> calculate(Iterable<MDVector> positions, MDVector systemBoundary) {
-
-		int totalNumOfParticles = IterableCounter.count(positions);
-
-		List<MDVector> listOfPositions;
-		if (positions instanceof Collection<?>) {
-			Collection<MDVector> collectionOfPositions = (Collection<MDVector>) positions;
-			listOfPositions = new ArrayList<MDVector>(collectionOfPositions);
-		} else {
-			listOfPositions = new ArrayList<>(totalNumOfParticles);
-			for (MDVector position : positions) {
-				listOfPositions.add(position);
-			}
-		}
-
-		listOfPositions.sort(new YComparator());
-
 		List<List<MDNumber>> density = new ArrayList<>(2);
 		List<MDNumber> yPositions = new ArrayList<>();
 		List<MDNumber> localDensity = new ArrayList<>();
